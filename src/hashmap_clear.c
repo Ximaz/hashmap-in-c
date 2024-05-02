8785d2943b617e619fb6d2d5cd34e88fa7e4e3fa
DURAND Malo <malo.durand@epitech.eu>
2024-05-02 08:09:02 +0200

    removing bad malloc

--- src/hashmap_clear.c
+++ src/hashmap_clear.c
@@ -5,16 +5,12 @@
 ** hashmap_clear.c
 */
 
-#include <stdlib.h>
-#include <stddef.h>
 #include "hashmap.h"
 
 void hashmap_clear(hashmap_t *hashmap)
 {
     size_t i = 0;
 
-    if (0 == malloc(1))
-        return;
     for (; i < HASHMAP_SIZE; ++i)
         hashmap_entries_destroy(&(hashmap->buckets[i]), hashmap->destroy);
 }
